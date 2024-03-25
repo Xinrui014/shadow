7f778fe91cfff68e0cd415761adda569100812ba
WangXinrui <wang_xinrui@hotmail.com>
2024-03-25 14:49:56 +0800

    training with gradient accumulation 3x4

--- train.py
+++ train.py
@@ -13,6 +13,8 @@ from model.sam_adapter.datasets import TrainDataset
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingLR
+import wandb
 
 
 if __name__ == "__main__":
@@ -34,7 +36,7 @@ if __name__ == "__main__":
     torch.backends.cudnn.benchmark = True
 
     Logger.setup_logger(None, opt['path']['log'],
-                        'train', level=logging.INFO, screen=True)
+                        'train', level=logging.INFO, screen=False)
     logger = logging.getLogger('base')
     logger.info(Logger.dict2str(opt))
 
@@ -44,7 +46,10 @@ if __name__ == "__main__":
     torch.manual_seed(1234)
     torch.cuda.manual_seed_all(1234)
 
-    # dataset
+    # init wandb
+    wandb.init(project='samshadow')
+
+    # init dataset for both diffusion and SAM-adapter
     for phase, dataset_opt in opt['datasets'].items():
         if phase == 'train' and args.phase != 'val':
             train_set = Data.create_dataset(dataset_opt, phase)
@@ -52,16 +57,22 @@ if __name__ == "__main__":
                 train_set, dataset_opt, phase)
     logger.info('Initial Dataset Finished')
 
-    # model
+    # init and load diffusion model
     diffusion = Model.create_model(opt)
     logger.info('Initial Model Finished')
 
-    # Train
+    # Training parameter
     current_step = diffusion.begin_step
     current_epoch = diffusion.begin_epoch
     n_iter = opt['train']['n_iter']
+    diffusion.set_new_noise_schedule(
+        opt['model']['beta_schedule'][opt['phase']], schedule_phase=opt['phase'])
+
 
     # Init and load SAM_adapter
+
+    # SAM-adapter epoch and min_learning rate
+    max_epoch = 200
     image_path = "/home/xinrui/projects/SAM-Adapter/data/ISTD_Dataset/train/train_A"
     mask_path = "/home/xinrui/projects/SAM-Adapter/data/ISTD_Dataset/train/train_B"
     datasets = TrainDataset(image_folder=image_path, mask_folder=mask_path)
@@ -70,7 +81,6 @@ if __name__ == "__main__":
 
     device = torch.device('cuda')
     adp_model = SAM(inp_size=1024, loss='iou').to(device)
-    # print(model)
     sam_checkpoint = torch.load("./experiments/official_test/SAM_adapter_ckpt/model_epoch_best.pth")
     adp_model.load_state_dict(sam_checkpoint, strict=False)
     for name, para in adp_model.named_parameters():
@@ -82,29 +92,39 @@ if __name__ == "__main__":
     # Initialize the AdamW optimizer for adp_model
     lr = 0.0002
     weight_decay = 0.0  # Set the weight decay value as needed
-    adp_model_optimizer = optim.AdamW(adp_model.parameters(), lr=lr, weight_decay=weight_decay)
+    # adp_model_optimizer = optim.AdamW(adp_model.parameters(), lr=lr, weight_decay=weight_decay)
 
-    # Set the optimizer in adp_model
-    adp_model.optimizer = adp_model_optimizer
+    adp_params = [{'params': adp_model.parameters(), 'lr': 0.0002}]
+    diffusion_params = [{'params': diffusion.netG.parameters(), 'lr': 3e-05}]
+    params = adp_params + diffusion_params
+    optimizer = optim.AdamW(params)
+    lr_scheduler = CosineAnnealingLR(optimizer, max_epoch, eta_min=1e-7)
 
-    diffusion.set_new_noise_schedule(
-        opt['model']['beta_schedule'][opt['phase']], schedule_phase=opt['phase'])
     # print name paramter in diffusion and the sam model
-    for name, para in adp_model.named_parameters():
-        # print layer name and if it requires grad and if it is a leaf tensor
-        # check if the tensor is not leaf but requires gradient
-        if para.requires_grad and not para.is_leaf:
-            print(name, para.requires_grad, para.is_leaf)
-    for name, para in diffusion.netG.named_parameters():
-        if para.requires_grad and not para.is_leaf:
-            print(name, para.requires_grad, para.is_leaf)
+    # for name, para in adp_model.named_parameters():
+    #     # print layer name and if it requires grad and if it is a leaf tensor
+    #     # check if the tensor is not leaf but requires gradient
+    #     if para.requires_grad and not para.is_leaf:
+    #         print(name, para.requires_grad, para.is_leaf)
+    # for name, para in diffusion.netG.named_parameters():
+    #     if para.requires_grad and not para.is_leaf:
+    #         print(name, para.requires_grad, para.is_leaf)
+
+    # Set number of iterations to accumulate gradients
+    gradient_accumulation_steps = 3
+
+    # training
     if opt['phase'] == 'train':
-        n_epoch = 20000
-        with tqdm(total=n_epoch, unit='epoch') as pbar:
-            while current_epoch < n_epoch:
+        # n_epoch = current_epoch+max_epoch
+        epoch = 0
+        with tqdm(total=max_epoch, unit='epoch', position=0) as pbar:
+            # while current_epoch < n_epoch:
+            #     current_epoch += 1
+            while epoch < max_epoch:
+                epoch += 1
                 current_epoch += 1
-                for _, train_data in enumerate(train_loader):
-                    current_step += 1
+                for index, train_data in tqdm(enumerate(train_loader), total=len(train_loader), unit='batch', position=1):
+                    # current_step += 1
                     if current_step > n_iter:
                         break
 
@@ -118,8 +138,8 @@ if __name__ == "__main__":
                     inp = F.interpolate(inp, size=(1024, 1024), mode='bilinear', align_corners=False)
                     gt = F.interpolate(gt, size=(1024, 1024), mode='bilinear', align_corners=False)
 
-                    diffusion.netG.zero_grad()
-                    adp_model.zero_grad()
+                    # diffusion.netG.zero_grad()
+                    # adp_model.zero_grad()
 
                     with torch.autocast(device_type="cuda"):
                         adp_model.set_input(inp, gt)
@@ -127,48 +147,50 @@ if __name__ == "__main__":
                         train_data['mask'] = F.interpolate(pred_mask, size=(160,160), mode='bilinear', align_corners=False)
                         diffusion.feed_data(train_data)
 
-                        # Combine the two losses
                         l_pix = diffusion.netG(diffusion.data)
                         b, c, h, w = diffusion.data['HR'].shape
                         l_pix = l_pix.sum() / int(b * c * h * w)
-                        # l_total = l_pix + adp_model.compute_loss()  # Assuming adp_model has a compute_loss() method
-
+                        l_pix = l_pix/gradient_accumulation_steps
                         # Perform a single backward pass
                         l_pix.backward()
-                    diffusion_gard = diffusion.netG.named_parameters()
-                    SAM_gard = adp_model.named_parameters()
+                        if (index + 1) % gradient_accumulation_steps == 0:
+                            current_step += 1
+                            optimizer.step()
+                            lr_scheduler.step()
+                            optimizer.zero_grad()
+                            diffusion.ema_helper.update(diffusion.netG)
+                        # lr_scheduler.step()
+                        # print the current learning rate
+                        # Get the current learning rate for each parameter group
+                        # for i, param_group in enumerate(optimizer.param_groups):
+                        #     current_lr = param_group['lr']
+                        #     print(f"Current learning rate for parameter group {i}: {current_lr}")
 
-                    # Update the optimizers
-                    diffusion.optG.step()
-                    # adp_model.optimizer.step()
-                    #
-                    # print("Diffusion model gradients:")
-                    # for name, param in diffusion.netG.named_parameters():
-                    #     print(f"{name}: {param.grad}")
-                    #     break
-                    #
-                    # print("SAM-adapter model gradients:")
-                    # for name, param in adp_model.named_parameters():
-                    #     print(f"{name}: {param.grad}")
-                    #     break
+                # Set log
+                wandb.log({'loss': l_pix.item()})
+                diffusion.log_dict['l_pix'] = l_pix.item()
 
-                    diffusion.ema_helper.update(diffusion.netG)
 
-                    # Set log
-                    diffusion.log_dict['l_pix'] = l_pix.item()
+                # log
+                if epoch % 10 == 0:
+                    logs = diffusion.get_current_log()
+                    message = '<epoch:{:3d}, iter:{:8,d}> '.format(
+                        current_epoch, current_step)
+                    for k, v in logs.items():
+                        message += '{:s}: {:.4e} '.format(k, v)
+                    logger.info(message)
 
-                    # log
-                    if current_step % opt['train']['print_freq'] == 0:
-                        logs = diffusion.get_current_log()
-                        message = '<epoch:{:3d}, iter:{:8,d}> '.format(
-                            current_epoch, current_step)
-                        for k, v in logs.items():
-                            message += '{:s}: {:.4e} '.format(k, v)
-                        logger.info(message)
-
-                    if current_step % opt['train']['save_checkpoint_freq'] == 0:
-                        logger.info('Saving models and training states.')
-                        diffusion.save_network(current_epoch, current_step)
+                if epoch % 50 == 0:
+                    logger.info('Saving models and training states.')
+                    diffusion.save_network(current_epoch, current_step)
+                    # save SAM_adapter
+                    sam_adapter_ckpt = {
+                        'epoch': epoch,
+                        'model_state_dict': adp_model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                    }
+                    torch.save(sam_adapter_ckpt, os.path.join(opt['path']['checkpoint'], f"adp_model_epoch_{epoch}.pth"))
 
                 pbar.update(1)
             logger.info('End of training.')
+            wandb.finish()
